b5be28370f6959a1ace16968be378a8af659ddad
Ikhwannt <156688115+Ikhwannt@users.noreply.github.com>
2025-12-17 18:55:56 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -18,17 +18,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-# TRAIN MODEL
-mlflow.set_experiment("CI-Retraining-Breast-Cancer")
+# TRAIN MODEL (MLflow Project Run)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-with mlflow.start_run():
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
+# LOGGING (TANPA start_run)
+mlflow.log_metric("accuracy", acc)
+mlflow.sklearn.log_model(model, "model")
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
-
-    print("Training selesai | Accuracy:", acc)
+print("Training selesai | Accuracy:", acc)
