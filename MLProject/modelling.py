184d35377d9e9956bde4079806c308be09a4afff
Ikhwannt <156688115+Ikhwannt@users.noreply.github.com>
2025-12-17 18:36:54 +0700

    Create modelling.py

---
+++ MLProject/modelling.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# LOAD DATASET (PREPROCESSED)
+df = pd.read_csv(
+    "breast_cancer_preprocessed.csv"
+)
+
+X = df.drop("target", axis=1)
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+# TRAIN MODEL
+mlflow.set_experiment("CI-Retraining-Breast-Cancer")
+
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+
+    print("Training selesai | Accuracy:", acc)
